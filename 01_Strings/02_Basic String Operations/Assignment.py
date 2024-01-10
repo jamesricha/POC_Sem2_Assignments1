06e0a3e23d8ca5eb0ba029f86a09687e37119ddb
James Richardson <richajam003@warren.k12.in.us>
2024-01-10 07:53:16 -0500

    Done

---
+++ 01_Strings/02_Basic String Operations/Assignment.py
@@ -0,0 +1,38 @@
+bit_of_text= ""
+
+for chr in bit_of_text:
+    pass
+
+print(len('I\'m James'))
+
+print(ord('j'))
+
+print(ord('J'))
+
+print(chr(106))
+
+print(chr(128512))
+
+test_string = '''Line 1
+ Line 2'''
+
+print(len(test_string))
+
+print('aaa' + 'bbb')
+
+print(2 * 'ccc')
+
+for char in 'hello from the world of Python':
+     print(char, end='-')
+
+text = 'dHello there!'
+del text[0]
+
+min('ilovetravellingaroundtheworld')
+
+min('i love travellingaroundtheworld')
+
+max('ilovetravellingaroundtheworld')
+
+
+
