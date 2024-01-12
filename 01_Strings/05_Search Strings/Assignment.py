3d3de28eb609dac555ca45bdcb6f269702e40e45
James Richardson <richajam003@warren.k12.in.us>
2024-01-12 07:58:55 -0500

    done

---
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -0,0 +1,37 @@
+print('iloveplaygames'.index('gaming'))
+
+my_text = 'This is my sample string'
+print(my_text.index('is'))
+print(my_text.find('if'))
+
+my_text = 'This is my sample string'
+print(my_text.index('are'))
+print(my_text.find('are'))
+
+my_text = 'This is my sample string'
+my_text.find('is', 10)
+
+my_text = 'This is my sample string'
+my_text.find('is', 10, 15)
+
+print('game game game'.find('game'))
+print('work work work'.rfind('game'))
+print('work work work'.find('game', 2))
+print('work work work'.rfind('game', 2))
+print('work work work'.find('game', 2, 5))
+print('work work work'.rfind('game', 2, 5))
+
+print('James'.isalnum())
+
+print('James15'.isalnum())
+
+print('James 15'.isalnum())
+
+print('James_15'.isalnum())
+
+print('James'.isalpha())
+
+print('James'.isdigit())
+
+
+
