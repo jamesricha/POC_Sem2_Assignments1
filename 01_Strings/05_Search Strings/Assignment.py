54fbbae21c16d65b24c087f7b9e4c29f6fc0fab5
James Richardson <richajam003@warren.k12.in.us>
2024-01-12 08:03:51 -0500

    Done

--- 01_Strings/05_Search Strings/Assignment.py
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -1,37 +1,8 @@
-print('iloveplaygames'.index('gaming'))
+text = 'iloveplayinggames'.index('gaming')
 
 my_text = 'This is my sample string'
-print(my_text.index('is'))
-print(my_text.find('if'))
-
-my_text = 'This is my sample string'
-print(my_text.index('are'))
-print(my_text.find('are'))
-
-my_text = 'This is my sample string'
-my_text.find('is', 10)
-
-my_text = 'This is my sample string'
-my_text.find('is', 10, 15)
-
-print('game game game'.find('game'))
-print('work work work'.rfind('game'))
-print('work work work'.find('game', 2))
-print('work work work'.rfind('game', 2))
-print('work work work'.find('game', 2, 5))
-print('work work work'.rfind('game', 2, 5))
-
-print('James'.isalnum())
-
-print('James15'.isalnum())
-
-print('James 15'.isalnum())
-
-print('James_15'.isalnum())
-
-print('James'.isalpha())
-
-print('James'.isdigit())
-
-
-
+print(my_text.find('the'))
+if result == -1:
+    print("The word is not in the string")
+else:
+    print("The word is in the string")
